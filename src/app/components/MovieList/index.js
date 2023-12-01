71e6e1dc8430bad53b35ca292086cb48e7a12de7
jasonkencana1 <jasonalfonza.kencana@edu.sait.ca>
2023-12-01 13:18:11 -0700

    Added reusable code

--- src/app/components/MovieList/index.js
+++ src/app/components/MovieList/index.js
@@ -1,12 +1,26 @@
 import Image from "next/image"
+import Link from "next/link"
 
-const MovieList = ({title, images}) => {
+const MovieList = ({ movies }) => {
     return (
-            <div className="bg-indigo-500">
-                <Image src={images} alt="..." width={600} height={600} />
-                <h3>{title}</h3>
-            </div>
-    )
+        <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
+            {movies.map((movie) => (
+                <div key={movie.mal_id} className="shadow-xl">
+                <Link href={`/${movie.mal_id}`} className="cursor-pointer">
+                    <Image 
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                        alt="..." 
+                        width={500} 
+                        height={700} 
+                        className="w-full max-h-64 object-cover"
+                    />
+
+                <h3 className="font-bold md:text-l text-sm p-4">{movie.title}</h3>
+                </Link>
+        </div>
+        ))}
+    </div>
+    );
 }
 
 export default MovieList
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
+import Header from "./components/MovieList/Header";
 import MovieList from "./components/MovieList"
 require('dotenv').config();
 
 const Home = async () =>  {
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 const apiKey = process.env.API_KEY;
-  
 const response = await fetch(`${apiBaseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
+const topMovie = await response.json()
 
-const movie = await response.json()
-console.log(movie.results)
-console.log(response.headers);
 
   return (
-    <div>
-      <h1>Most Popular</h1>
-        {movie.results.map(results => {
-          return <MovieList title={results.title} images={`https://image.tmdb.org/t/p/w500${results.poster_path}`} />
-        })}
-    </div>
+    <>
+    {/* Most Popular Components */}
+      <section>
+        <Header title="Most Popular" linkTitle="All Movies" linkHref="/popular"/>
+        <MovieList movies={topMovie.results}/>
+      </section>
+
+    {/* Latest Components */}
+      <section>
+        <Header title="Latest Movie" linkTitle="All Movies" linkHref="/new"/>
+        <MovieList movies={topMovie.results}/>
+      </section>
+    </>
   )
 }
 
